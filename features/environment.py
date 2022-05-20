e4f0fbe578fb46d331c79d64092bcb414c5f5b1e
Yasir Satti <yassir_satti@hotmail.com>
2022-05-20 11:18:11 +0100

    registration submit works and refactoring

--- features/environment.py
+++ features/environment.py
@@ -5,21 +5,32 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
-from features.pageobjects.BasePage import BasePage
+from features.pageobjects.CommonPage import CommonPage
+from features.pageobjects.InputRegistrationPage import InputRegistrationPage
+
 
 def before_all(context):
-    
+
     userdata = context.config.userdata
 
     if userdata['browser'] == "chrome":
         options = webdriver.ChromeOptions()
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
-        context.driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
-    
+        context.driver = webdriver.Chrome(
+            options=options, executable_path=ChromeDriverManager().install())
+
     if userdata['browser'] == "firefox":
-        context.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-        
+        context.driver = webdriver.Firefox(
+            executable_path=GeckoDriverManager().install())
+
     context.baseUrl = userdata["baseUrl"]
-    context.basePage = BasePage(context.driver)
+    context.inputRegistrationPage = InputRegistrationPage(context.driver)
+    context.commonPage = CommonPage(context.driver)
+
+
+
+
+
+
 
--- features/steps/registration.py
+++
@@ -1,5 +0,0 @@
-from behave import step
-
-@step('I make registration')
-def step_impl(context):
-    print('registration....')
