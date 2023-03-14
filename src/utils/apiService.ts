d138c2c86cc94405f91dbaeb234a396cf025429d
yasir satti <yassir.satti@hotmail.com>
2023-03-14 23:20:16 +0000

    API create service

--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -2,7 +2,7 @@ import { type SignUpFormData } from '../pages/SignUpPage/SignUpPage.schema';
 
 // const URL = 'http://localhost:5001/api/registration';
 const URL =
-	'https://env-backendbookrentalapp.eba-mmucheux.eu-west-2.elasticbeanstalk.com/api/registration';
+	'http://env-backendbookrentalapp.eba-mmucheux.eu-west-2.elasticbeanstalk.com/api/registration';
 
 const headers = {
 	'Content-Type': 'application/json',
@@ -18,25 +18,5 @@ const apiService = {
 		return await response.json();
 	},
 };
-
-// import { RegistrationFormData } from "../pages/RegistrationPage/RegistrationPage.schema";
-// import axios from 'axios';
-
-// // const url = 'http://localhost:5001/api/registration';
-// const url = 'http://env-backendbookrentalapp.eba-mmucheux.eu-west-2.elasticbeanstalk.com/api/registration';
-
-// const apiService = {
-
-//     create: async (payload: Partial<RegistrationFormData>) => {
-//         axios.post(url, payload)
-//         //   .then((response) => {
-//         //     if (response.data != null) {
-//         //         alert("Registration data posting was successful!");
-//         //     } else {
-//         //         alert("Registration data posting ERROR!");
-//         //     }
-//         // });
-//     },
-// }
 
 export default apiService;
